58c80012f437116b03e41d0bec22bb6efd22dc28
Ismaildwin <101150684+Ismaildwi@users.noreply.github.com>
2022-10-24 21:35:18 +0700

    variabel

---
+++ variabel/index.js
@@ -0,0 +1,25 @@
+//variabel ada 3 dijavascript
+// fungsi variabel adalah untuk menyimpan data (array, number, integer, boolean, objek, string)
+//const -> 
+//let ->
+//var ->
+
+//string -> ""/''
+//number/integer -> 12
+//boolean ->  true/false
+
+//array -> []
+//object -> {}.
+
+//ketika ga pengen nilai variabelnya berubah maka gunakan const
+const nama = "ismaildwin";
+nama = "ardis"
+
+//ketika pengen nilai dari variabelnya berubah-ubah, gunakan let
+let umur = 16;
+umur = 15;
+
+//ketika mau membuat variabel dengan nama yang sama, maka gunakan var
+var jenisKelamin = "cowo";
+var jenisKelamin = "cewe";
+console.log (jenisKelamin)
